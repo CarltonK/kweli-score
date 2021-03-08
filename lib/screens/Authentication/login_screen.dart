9d7af0dac24fa78f308714844a71846e7d9ef673
Eric Kinyua <eriquekinyua0@gmail.com>
2021-03-08 11:16:18 +0300

    Created a Package Pages

--- lib/screens/Authentication/login_screen.dart
+++ lib/screens/Authentication/login_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:kweliscore/helpers/helpers.dart';
+import 'package:kweliscore/screens/package.dart';
 import 'package:kweliscore/utilities/utilities.dart';
 
 class Login extends StatelessWidget {
@@ -10,88 +11,121 @@ class Login extends StatelessWidget {
     // Dimensions
     Size size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Container(
-        height: size.height,
-        width: size.width,
-        padding: const EdgeInsets.only(top: 50, left: 20, right: 20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const SizedBox(height: 60),
-            Text('Kweli Score', style: Constants.boldHeadlineStyle),
-            const SizedBox(height: 40),
-            TextFormField(
-              keyboardType: TextInputType.text,
-              textInputAction: TextInputAction.next,
-              validator: _validator.emailValidator,
-              decoration: InputDecoration(
-                border: Constants.blackInputBorder,
-                enabledBorder: Constants.blackInputBorder,
-                focusedBorder: Constants.blackInputBorder,
-                labelText: 'Email',
-                prefixIcon: Icon(Icons.email),
-              ),
-            ),
-            const SizedBox(height: 20),
-            TextFormField(
-              keyboardType: TextInputType.text,
-              textInputAction: TextInputAction.next,
-              validator: _validator.passwordValidator,
-              decoration: InputDecoration(
-                border: Constants.blackInputBorder,
-                enabledBorder: Constants.blackInputBorder,
-                focusedBorder: Constants.blackInputBorder,
-                labelText: 'Password',
-                prefixIcon: Icon(Icons.vpn_key),
-                suffixIcon: IconButton(
-                  icon: Icon(Icons.remove_red_eye),
-                  onPressed: null,
+      body: SingleChildScrollView(
+        child: Container(
+          height: size.height,
+          width: size.width,
+          padding: const EdgeInsets.only(top: 50, left: 20, right: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(height: 60),
+              Text('Kweli Score', style: Constants.boldHeadlineStyle),
+              const SizedBox(height: 40),
+              TextFormField(
+                keyboardType: TextInputType.text,
+                textInputAction: TextInputAction.next,
+                validator: _validator.emailValidator,
+                decoration: InputDecoration(
+                  border: Constants.blackInputBorder,
+                  enabledBorder: Constants.blackInputBorder,
+                  focusedBorder: Constants.blackInputBorder,
+                  labelText: 'Email',
+                  prefixIcon: Icon(Icons.email),
                 ),
               ),
-            ),
-            const SizedBox(height: 40),
-            MaterialButton(
-              minWidth: size.width,
-              elevation: 5,
-              height: 55,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(16),
-              ),
-              color: Colors.lightBlueAccent,
-              child: Text(
-                'Sign In',
-                style: Constants.whiteBoldSubheadlineStyle,
-              ),
-              onPressed: () => {},
-            ),
-            const SizedBox(height: 10),
-            FlatButton(
-              onPressed: () => {},
-              child: Text(
-                'Forgot Password',
-                style: Constants.blackBoldNormal,
-              ),
-            ),
-            const SizedBox(height: 60),
-            Text(
-              'Or Sign In with',
-              style: Constants.boldSubheadlineStyle,
-            ),
-            const SizedBox(height: 20),
-            Row(
-              children: [
-                Container(
-                  height: 50,
-                  width: 50,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: Colors.black,
+              const SizedBox(height: 20),
+              TextFormField(
+                keyboardType: TextInputType.text,
+                textInputAction: TextInputAction.next,
+                validator: _validator.passwordValidator,
+                decoration: InputDecoration(
+                  border: Constants.blackInputBorder,
+                  enabledBorder: Constants.blackInputBorder,
+                  focusedBorder: Constants.blackInputBorder,
+                  labelText: 'Password',
+                  prefixIcon: Icon(Icons.vpn_key),
+                  suffixIcon: IconButton(
+                    icon: Icon(Icons.remove_red_eye),
+                    onPressed: null,
                   ),
-                  // child: Image(image: Image.asset('assets/images/facebook.jpg')),
-                )
-              ],
-            ),
-          ],
+                ),
+              ),
+              const SizedBox(height: 40),
+              MaterialButton(
+                minWidth: size.width,
+                elevation: 5,
+                height: 55,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                color: Colors.lightBlueAccent,
+                child: Text(
+                  'Sign In',
+                  style: Constants.whiteBoldSubheadlineStyle,
+                ),
+                onPressed: () => {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => Package()),
+                  )
+                },
+              ),
+              const SizedBox(height: 10),
+              FlatButton(
+                onPressed: () => {},
+                child: Text(
+                  'Forgot Password?',
+                  style: Constants.blackBoldNormal,
+                ),
+              ),
+              const SizedBox(height: 60),
+              Text(
+                'Or Sign In with',
+                style: Constants.boldSubheadlineStyle,
+              ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                    height: 50,
+                    width: 50,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                    ),
+                    child: Image(
+                        image: AssetImage('assets/images/facebook.jpg'),
+                        fit: BoxFit.contain),
+                  ),
+                  Container(
+                    height: 50,
+                    width: 50,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                    ),
+                    child: Image(
+                      image: AssetImage('assets/images/google.png'),
+                      fit: BoxFit.contain,
+                    ),
+                  ),
+                  Container(
+                    height: 50,
+                    width: 50,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                    ),
+                    child: Image(
+                      image: AssetImage(
+                        'assets/images/twitter.png',
+                      ),
+                      fit: BoxFit.contain,
+                    ),
+                  )
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
---
+++ lib/screens/package.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:kweliscore/screens/Payment%20Packages/free_package.dart';
+import 'package:kweliscore/utilities/utilities.dart';
+
+class Package extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.only(top: 60, left: 20, right: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(
+              height: 20,
+            ),
+            Text(
+              'Please select your payment plan,',
+              style: Constants.boldSubheadlineStyle,
+            ),
+            const SizedBox(
+              height: 40,
+            ),
+            GestureDetector(
+              onTap: () => {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => FreePackage()),
+                )
+              },
+              child: Container(
+                  height: 170,
+                  width: 340,
+                  padding: EdgeInsets.all(40),
+                  decoration: BoxDecoration(
+                      color: Colors.black12,
+                      borderRadius: BorderRadius.circular(16)),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [Text('Free'), Text('Ksh 0')],
+                  )),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            GestureDetector(
+              child: Container(
+                  height: 170,
+                  width: 340,
+                  padding: EdgeInsets.all(40),
+                  decoration: BoxDecoration(
+                      color: Colors.black12,
+                      borderRadius: BorderRadius.circular(16)),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [Text('Monthly'), Text('Ksh 28')],
+                  )),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            GestureDetector(
+              child: Container(
+                  height: 170,
+                  width: 340,
+                  padding: EdgeInsets.all(40),
+                  decoration: BoxDecoration(
+                      color: Colors.black12,
+                      borderRadius: BorderRadius.circular(16)),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [Text('Yearly'), Text('Ksh 250')],
+                  )),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
