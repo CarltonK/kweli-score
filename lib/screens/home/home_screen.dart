af9eff5305aada35c22fba669fef06101641ab73
Eric Kinyua <eriquekinyua0@gmail.com>
2021-03-11 09:03:55 +0300

    Created the homepage shell

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -9,7 +9,43 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: AppBar(
+        title: Text(
+          'KWELI SCORE',
+        ),
+      ),
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.only(top: 50, left: 20, right: 20),
+        child: Column(
+          children: [
+            Container(
+              height: 350,
+              width: 340,
+              decoration: BoxDecoration(
+                color: Colors.black12,
+                borderRadius: BorderRadius.circular(16),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Container(
+              height: 200,
+              width: 340,
+              decoration: BoxDecoration(
+                color: Colors.black12,
+                borderRadius: BorderRadius.circular(16),
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => {},
+        child: IconButton(icon: Icon(Icons.add), onPressed: () => {}),
+      ),
     );
   }
 }
