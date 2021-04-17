7eb2f024c0ca7a9cb36cd68be3ec4a7df7ef30a1
Mark <mcarlton33@gmail.com>
2021-04-17 11:15:01 +0200

    initialize firebase app at the very top

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
-// import 'dart:async';
+import 'dart:async';
 import 'package:firebase_analytics/observer.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
-// import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:kweliscore/widgets/widgets.dart';
@@ -11,8 +11,10 @@ import 'package:provider/provider.dart';
 import 'package:provider/single_child_widget.dart';
 import 'provider/providers.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   final List<SingleChildWidget> providers = [
     ChangeNotifierProvider(
       create: (context) => AuthProvider.instance(),
@@ -25,26 +27,26 @@ void main() {
     ),
   ];
 
-  // runZonedGuarded(() {
-  runApp(
-    MultiProvider(
-      providers: providers,
-      child: MyApp(),
-    ),
-  );
-  // }, (error, stackTrace) {
-  //   FirebaseCrashlytics.instance.recordError(error, stackTrace);
-  // });
+  runZonedGuarded(() {
+    runApp(
+      MultiProvider(
+        providers: providers,
+        child: MyApp(),
+      ),
+    );
+  }, (error, stackTrace) {
+    FirebaseCrashlytics.instance.recordError(error, stackTrace);
+  });
 }
 
 class MyApp extends StatelessWidget {
   // Initialize firebase outside build to avoid future builder triggers
   // final Future<FirebaseApp> _initialization = Firebase.initializeApp();
 
-  // static FirebaseAnalytics analytics = FirebaseAnalytics();
-  // static FirebaseAnalyticsObserver observer = FirebaseAnalyticsObserver(
-  //   analytics: analytics,
-  // );
+  static FirebaseAnalytics analytics = FirebaseAnalytics();
+  static FirebaseAnalyticsObserver observer = FirebaseAnalyticsObserver(
+    analytics: analytics,
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -58,11 +60,23 @@ class MyApp extends StatelessWidget {
           Theme.of(context).textTheme,
         ),
       ),
-      // navigatorObservers: <NavigatorObserver>[observer],
-      home: BaseApp(),
+      navigatorObservers: <NavigatorObserver>[observer],
+      home: Consumer<AuthProvider>(
+        builder: (context, value, child) {
+          if (value.status == Status.Authenticated) return HomePage();
+          if (value.status == Status.Authenticating) return GlobalLoader();
+          return child;
+        },
+        child: MainAuthentication(),
+      ),
     );
   }
 }
+
+/*
+ * THIS IS A SECONDARY WAY OF INITIALIZING FIREBASE
+ * CURRENTLY NOT IN USE
+ */
 
 class BaseApp extends StatefulWidget {
   @override
