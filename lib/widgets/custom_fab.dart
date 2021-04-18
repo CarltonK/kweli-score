16c8ebc7f7b30f9150ee53feaad5e672f3bea206
Mark <mcarlton33@gmail.com>
2021-04-18 11:13:27 +0200

    restore file picker

--- lib/widgets/custom_fab.dart
+++ lib/widgets/custom_fab.dart
@@ -1,7 +1,7 @@
-// import 'dart:io';
-// import 'package:file_picker/file_picker.dart';
+import 'dart:io';
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
-// import 'package:flutter/services.dart';
+import 'package:flutter/services.dart';
 import 'package:kweliscore/provider/providers.dart';
 import 'package:provider/provider.dart';
 
@@ -30,11 +30,10 @@ class _CustomFabState extends State<CustomFab>
     isOpened = !isOpened;
   }
 
-  // File _statementFile;
+  File _statementFile;
   String urlResult, uid;
-  // List<String> _allowedExtentions;
-  // FileType _pickingType;
-  // StorageUploadTask storageUploadTask;
+  List<String> _allowedExtentions;
+  FileType _pickingType;
 
   @override
   void initState() {
@@ -43,8 +42,8 @@ class _CustomFabState extends State<CustomFab>
     // Get userId
     uid = context.read<AuthProvider>().currentUser.uid;
 
-    // _allowedExtentions = ['pdf'];
-    // _pickingType = FileType.custom;
+    _allowedExtentions = ['pdf'];
+    _pickingType = FileType.custom;
 
     _animationController = AnimationController(
       vsync: this,
@@ -89,28 +88,28 @@ class _CustomFabState extends State<CustomFab>
     );
   }
 
-  void fileChooserBtnPressed() => print('Button pressed');
+  void fileChooserBtnPressed() => pickFiles();
 
-  // pickFiles() async {
-  //   try {
-  //     FilePickerResult result = await FilePicker.platform.pickFiles(
-  //       type: _pickingType,
-  //       allowedExtensions: [..._allowedExtentions],
-  //     );
+  pickFiles() async {
+    try {
+      FilePickerResult result = await FilePicker.platform.pickFiles(
+        type: _pickingType,
+        allowedExtensions: [..._allowedExtentions],
+      );
 
-  //     if (result != null) {
-  //       _statementFile = File(result.files.single.path);
-  //       Provider.of<StorageProvider>(context, listen: false).startUpload(
-  //         _statementFile,
-  //         uid,
-  //       );
-  //     }
-  //   } on PlatformException catch (error) {
-  //     print("Platform Exception: " + error.toString());
-  //   } catch (error) {
-  //     print("Unsupported operation: " + error.toString());
-  //   }
-  // }
+      if (result != null) {
+        _statementFile = File(result.files.single.path);
+        Provider.of<StorageProvider>(context, listen: false).startUpload(
+          _statementFile,
+          uid,
+        );
+      }
+    } on PlatformException catch (error) {
+      print("Platform Exception: " + error.toString());
+    } catch (error) {
+      print("Unsupported operation: " + error.toString());
+    }
+  }
 
   Widget fileChooserBtn() {
     return Container(
