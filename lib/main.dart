91bc4660c2e58066c3bd3434d61182e2acfed1c6
Mark Carlton <mcarlton33@gmail.com>
2021-05-31 06:13:52 +0300

    remove unused providers

--- lib/main.dart
+++ lib/main.dart
@@ -17,12 +17,6 @@ void main() {
     ChangeNotifierProvider(
       create: (context) => AuthProvider.instance(),
     ),
-    Provider(
-      create: (context) => DatabaseProvider(),
-    ),
-    Provider(
-      create: (context) => StorageProvider(),
-    ),
   ];
 
   runZonedGuarded(() {
