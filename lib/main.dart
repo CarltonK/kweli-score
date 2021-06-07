d7bef37047bb5841b37388d366cfbc11913f9a27
Mark Carlton <mcarlton33@gmail.com>
2021-06-07 10:22:04 +0300

    KS-32 #comment slack test

--- lib/main.dart
+++ lib/main.dart
@@ -42,6 +42,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    print('Slack Test');
     return MaterialApp(
       title: 'K-Smart',
       debugShowCheckedModeBanner: false,
