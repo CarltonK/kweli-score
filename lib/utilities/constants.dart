00913d1df77ce462feccc66279074a63d0c96337
Mark Carlton <mcarlton33@gmail.com>
2021-06-08 21:43:07 +0300

    KS-41 #comment new configs

--- lib/utilities/constants.dart
+++ lib/utilities/constants.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:kweliscore/utilities/utilities.dart';
 
 class Constants {
   // Constructor
@@ -64,11 +66,15 @@ class Constants {
     borderSide: BorderSide(color: Colors.black),
   );
 
-  // Sizing
+  /*
+  SIZING
+  */
   static const double padding = 20;
   static const double avatarRadius = 45;
 
-  // Decorations
+  /*
+  DECORATIONS AND BORDERS
+  */
   static const BoxDecoration kBoxDecoration = BoxDecoration(
     color: Colors.greenAccent,
     boxShadow: [
@@ -79,4 +85,62 @@ class Constants {
       ),
     ],
   );
+
+  static OutlineInputBorder outlineInputBorder = OutlineInputBorder(
+    borderRadius: BorderRadius.circular(28),
+    borderSide: BorderSide(color: Palette.kTextColor),
+    gapPadding: 10,
+  );
+
+  static RoundedRectangleBorder curvedRectBorder =
+      RoundedRectangleBorder(borderRadius: BorderRadius.circular(20));
+
+  /*
+  THEME
+  */
+  static ThemeData appTheme = ThemeData(
+    scaffoldBackgroundColor: Colors.white,
+    platform: TargetPlatform.iOS,
+    fontFamily: 'Lato',
+    appBarTheme: appBarTheme,
+    inputDecorationTheme: inputDecorationTheme,
+    visualDensity: VisualDensity.adaptivePlatformDensity,
+  );
+
+  static AppBarTheme appBarTheme = AppBarTheme(
+    color: Colors.white,
+    elevation: 0,
+    brightness: Brightness.light,
+    iconTheme: IconThemeData(color: Colors.black),
+    textTheme: GoogleFonts.latoTextTheme(),
+  );
+
+  static TextTheme textTheme = GoogleFonts.latoTextTheme();
+
+  static InputDecorationTheme inputDecorationTheme = InputDecorationTheme(
+    floatingLabelBehavior: FloatingLabelBehavior.always,
+    contentPadding: const EdgeInsets.symmetric(horizontal: 42, vertical: 20),
+    enabledBorder: outlineInputBorder,
+    focusedBorder: outlineInputBorder,
+    border: outlineInputBorder,
+  );
+
+  /*
+  REGEX
+  */
+  static RegExp emailValidatorRegExp =
+      RegExp(r"^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+
+  /*
+  FORM ERRORS
+  */
+  static const String kIdNumberNullError =
+      'Please enter your Identification Number';
+  static const String kInvalidEmailError = 'Please enter Valid Email';
+  static const String kPassNullError = 'Please enter your password';
+  static const String kShortPassError = 'Password is too short';
+  static const String kMatchPassError = 'Passwords don\'t match';
+  static const String kNamelNullError = 'Please enter your name';
+  static const String kPhoneNumberNullError = 'Please enter your phone number';
+  static const String kAddressNullError = "Please enter your address";
 }
