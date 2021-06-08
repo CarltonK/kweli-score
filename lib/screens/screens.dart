0346ea4170bb7057aa95de92a125de0287c45e37
Mark Carlton <mcarlton33@gmail.com>
2021-06-08 21:44:55 +0300

    KS-41 #comment new auth screens

--- lib/screens/screens.dart
+++ lib/screens/screens.dart
@@ -5,6 +5,11 @@ export 'Authentication/forgotpassword_screen.dart';
 export 'Authentication/main_authentication.dart';
 export 'Authentication/instruction_screens.dart';
 export 'Authentication/registration_otp.dart';
+// New Auth Model
+export 'Authentication/sign_up/sign_up.dart';
+export 'Authentication/sign_in/sign_in.dart';
+export 'Authentication/otp/otp.dart';
+export 'Authentication/forgot_password/forgotPassword.dart';
 
 // Package
 export 'packages/package.dart';
@@ -12,8 +17,5 @@ export 'packages/package.dart';
 // Home
 export 'home/home_screen.dart';
 
-//onBoarding
-export 'Onboarding/on_boarding.dart';
-export 'Onboarding/intro_three.dart';
-export 'Onboarding/intro_one.dart';
-export 'Onboarding/intro_two.dart';
+//On-Boarding
+export 'onboarding/onboarding.dart';
