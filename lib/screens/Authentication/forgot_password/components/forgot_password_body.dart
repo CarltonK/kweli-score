23f45d8d7dcdcc04d73089428e04a2cc88561f82
Mark Carlton <mcarlton33@gmail.com>
2021-06-09 08:33:12 +0300

    KS-41 #comment hide keyboard when screen is tapped

--- lib/screens/Authentication/forgot_password/components/forgot_password_body.dart
+++ lib/screens/Authentication/forgot_password/components/forgot_password_body.dart
@@ -6,27 +6,30 @@ class ForgotPasswordBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: SizedBox(
-        width: double.infinity,
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: EdgeInsets.symmetric(
-                horizontal: getProportionateScreenWidth(20)),
-            child: Column(
-              children: [
-                SizedBox(height: DeviceConfig.screenHeight! * 0.04),
-                Text(
-                  'Forgot Password',
-                  style: Constants.kHeadlineStyle,
-                ),
-                SizedBox(height: DeviceConfig.screenHeight! * 0.005),
-                Text(
-                  'Please enter your identification number and we will send \nyou a link to return to your account',
-                  textAlign: TextAlign.center,
-                ),
-                SizedBox(height: DeviceConfig.screenHeight! * 0.08),
-                ForgotPasswordForm(),
-              ],
+      child: GestureDetector(
+        onTap: () => KeyboardUtil.hideKeyboard(context),
+        child: SizedBox(
+          width: double.infinity,
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.symmetric(
+                  horizontal: getProportionateScreenWidth(20)),
+              child: Column(
+                children: [
+                  SizedBox(height: DeviceConfig.screenHeight! * 0.04),
+                  Text(
+                    'Forgot Password',
+                    style: Constants.kHeadlineStyle,
+                  ),
+                  SizedBox(height: DeviceConfig.screenHeight! * 0.005),
+                  Text(
+                    'Please enter your identification number and we will send \nyou a link to return to your account',
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(height: DeviceConfig.screenHeight! * 0.08),
+                  ForgotPasswordForm(),
+                ],
+              ),
             ),
           ),
         ),
--- lib/screens/Authentication/sign_in/components/sign_in_body.dart
+++ lib/screens/Authentication/sign_in/components/sign_in_body.dart
@@ -7,39 +7,42 @@ class SignInBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: SizedBox(
-        width: double.infinity,
-        child: Padding(
-          padding: EdgeInsets.symmetric(
-            horizontal: getProportionateScreenWidth(20),
-          ),
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                SizedBox(height: DeviceConfig.screenHeight! * 0.04),
-                Text(
-                  'Welcome Back',
-                  style: Constants.kHeadlineStyle,
-                ),
-                SizedBox(height: DeviceConfig.screenHeight! * 0.005),
-                Text(
-                  'Sign in with your identification number and password',
-                  textAlign: TextAlign.center,
-                ),
-                SizedBox(height: DeviceConfig.screenHeight! * 0.08),
-                SignInForm(),
-                SizedBox(height: DeviceConfig.screenHeight! * 0.08),
-                GlobalMultiInfoActionButton(
-                  primaryText: 'Don\'t have an account ?',
-                  secondaryText: 'Sign Up',
-                  onTap: () => Navigator.of(context).push(
-                    SlideLeftTransition(
-                      page: SignUpScreen(),
-                      routeName: 'sign_up_screen',
+      child: GestureDetector(
+        onTap: () => KeyboardUtil.hideKeyboard(context),
+        child: SizedBox(
+          width: double.infinity,
+          child: Padding(
+            padding: EdgeInsets.symmetric(
+              horizontal: getProportionateScreenWidth(20),
+            ),
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  SizedBox(height: DeviceConfig.screenHeight! * 0.04),
+                  Text(
+                    'Welcome Back',
+                    style: Constants.kHeadlineStyle,
+                  ),
+                  SizedBox(height: DeviceConfig.screenHeight! * 0.005),
+                  Text(
+                    'Sign in with your identification number and password',
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(height: DeviceConfig.screenHeight! * 0.08),
+                  SignInForm(),
+                  SizedBox(height: DeviceConfig.screenHeight! * 0.08),
+                  GlobalMultiInfoActionButton(
+                    primaryText: 'Don\'t have an account ?',
+                    secondaryText: 'Sign Up',
+                    onTap: () => Navigator.of(context).push(
+                      SlideLeftTransition(
+                        page: SignUpScreen(),
+                        routeName: 'sign_up_screen',
+                      ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
